43b38faf850dd099024e021ff13b9823443e820e
seshuvardhan <seshuvadhan825@gma.om>
2025-08-06 21:11:34 +0530

    removed hashrouter import in app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // import logo from "./logo.svg";
 import "./App.css";
 import React from "react";
-import { BrowserRouter,HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
 import Nav from "./components/Nav";
